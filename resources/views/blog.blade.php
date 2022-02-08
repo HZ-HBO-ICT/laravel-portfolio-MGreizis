23e054111ff387ab9f5ba3ffaa972655ac9d9a5c
Martins Greizis <grei0003@hz.nl>
2022-02-08 15:27:07 +0100

    Personal website added

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/blogstyle.css">
+    <link rel="icon" href="img/CatWithMask.jpg">
+    <title>Blog</title>
+</head>
+<body>
+
+<header>
+    <ul class="bar">
+        <li><a href="/">Home</a></li>
+        <li><a href="profile">Profile</a></li>
+        <li><a href="dashboard">Dashboard</a></li>
+        <li><a href="faq">FAQ</a></li>
+        <li><a href="blog" class="active">Blog</a></li>
+    </ul> <br>
+    <div class="designer">
+        <b>Designed by MG, 2021</b>
+    </div>
+</header>
+
+<main>
+    <article class="studychoice">
+        <h2>Study choice</h2>
+        <section>
+            <b><p>A combination of the sections <i>study choice activities</i>, <i>motivate your study choice</i> and <i>after completing the study</i> from my SKC document</p>
+                <p><a href="studychoice.html" class="readmore">Read more about it here...</a></p></b>
+        </section>
+    </article>
+
+    <article class="swot">
+        <h2>Personal SWOT analysis</h2>
+        <section>
+            <b><p>By making a SWOT-analysis, I analyze my personal strengths and weaknesses and make an inventory of the opportunities and threats I expect in my studies</p>
+                <p><a href="swotanalysis.html" class="readmore">Read more about it here...</a></p></b>
+        </section>
+    </article>
+
+    <article class="programmingexperience">
+        <h2>Programming experience</h2>
+        <section>
+            <b><p>Do I have any prior programming experience?</p>
+                <p><a href="programexperience.html" class="readmore">Read more about it here...</a></p></b>
+        </section>
+    </article>
+
+    <article class="firstfeedback">
+        <h2>First feedback</h2>
+        <section>
+            <b><p>My first SKC feedback and my thoughts on it</p>
+                <p><a href="firstfeedback.html" class="readmore">Read more about it here...</a></p></b>
+        </section>
+    </article>
+
+    <article class="profession">
+        <h2>Profession</h2>
+        <section>
+            <b><p>What is technology heading towards? What are some notable companies working on advancing technology?</p>
+                <p><a href="profession.html" class="readmore">Read more about it here...</a></p></b>
+        </section>
+    </article>
+</main>
+
+<footer>
+    <table>
+        <tr>
+            <td><a href="https://hz.nl/uploads/documents/Regelingen/OERS/2021-2022/HZ-Course-and-Examination-Regulations-2021-2022.pdf" class="footerlink">Course and Examination Regulations</a></td>
+            <td><a href="https://hz.nl/uploads/documents/Regelingen/OERS/2020-2021/9.-Implementation-Regulations-ICT-Fulltime-2020-2021.pdf" class="footerlink">Implementation Regulations</a></td>
+            <td><a href="https://learn.hz.nl/my/" class="footerlink">The Learn Environment</a></td>
+        </tr>
+
+        <tr>
+            <td><a href="https://teams.microsoft.com/l/team/19%3a2e2afa0286b04932be16cb8ad2d9d2c0%40thread.skype/conversations?groupId=95bddebc-a340-4d88-81fc-b80e0bfc70c3&tenantId=4c16deb3-342d-4fca-bcd5-b1429308034c" class="footerlink">The Teams Environment</a></td>
+            <td><a href="https://apps.hz.nl/angular/studievoortgang/studiestatus" class="footerlink">MyHZ Study progress</a></td>
+            <td><a href="https://github.com/MGreizis/mgreizis.github.io" class="footerlink">GitHub Environment</a></td>
+        </tr>
+    </table>
+</footer>
+</body>
+</html>
