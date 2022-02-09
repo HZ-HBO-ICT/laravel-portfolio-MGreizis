0ba921899f1f826e52de672399125513c905cb5f
Martins Greizis <grei0003@hz.nl>
2022-02-09 10:15:28 +0100

    Added comments to show methods

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 class BlogController
 {
-    public function show() {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function show()
+    {
         return view('blog');
     }
 }
