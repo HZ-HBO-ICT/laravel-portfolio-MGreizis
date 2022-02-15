cdcfe564d744341c2de883c1608bfe9599e5b657
Martins Greizis <grei0003@hz.nl>
2022-02-15 10:07:35 +0100

    Fixed phpcs errors

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use DB;
 use App\Models\Faq;
 
--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Contracts\Facades\DB;
 
 class PostsController extends Controller
 {
+    /**
+     * Shows the contents of the table
+     * @param $slug
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function show($slug)
     {
         $post = DB::table('posts')->where('slug', $slug)->first();
@@ -14,15 +20,19 @@ class PostsController extends Controller
         ]);
     }
 
+    /**
+     * Creates a table
+     * @return void
+     */
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-           $table->id();
-           $table->string('slug');
-           $table->string('title');
-           $table->text('body');
-           $table->timestamps();
-           $table->timestamp('published_at')->nullable();
+            $table->id();
+            $table->string('slug');
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
         });
     }
 }
